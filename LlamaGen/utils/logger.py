2e562b56e0aca2992da32ca34931dad9e01ab4ec
Yiran <yiranzhang3502@outlook.com>
2026-04-02 09:37:31 +0800

    adapt to MetaX

--- LlamaGen/utils/logger.py
+++ LlamaGen/utils/logger.py
@@ -1,16 +1,21 @@
 import logging
 import torch.distributed as dist
 
+
 def create_logger(logging_dir):
     """
     Create a logger that writes to a log file and stdout.
     """
-    if dist.get_rank() == 0:  # real logger
+    is_rank0 = (not dist.is_available()) or (not dist.is_initialized()) or dist.get_rank() == 0
+    if is_rank0:  # real logger
+        handlers = [logging.StreamHandler()]
+        if logging_dir is not None:
+            handlers.append(logging.FileHandler(f"{logging_dir}/log.txt"))
         logging.basicConfig(
             level=logging.INFO,
             format='[\033[34m%(asctime)s\033[0m] %(message)s',
             datefmt='%Y-%m-%d %H:%M:%S',
-            handlers=[logging.StreamHandler(), logging.FileHandler(f"{logging_dir}/log.txt")]
+            handlers=handlers,
         )
         logger = logging.getLogger(__name__)
     else:  # dummy logger (does nothing)
